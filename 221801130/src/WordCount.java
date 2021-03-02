7ca66dc74e83bc00aae2c436fc6082034a9f3ae8
liangyangxin <965720407@qq.com>
2021-03-02 16:22:13 +0800

    更进字符统计功能和新增单词统计功能

--- 221801130/src/WordCount.java
+++ 221801130/src/WordCount.java
@@ -12,16 +12,42 @@ public class WordCount {
 		Scanner input = new Scanner(System.in);
 		String path = input.next();
 		int countChar = 0;
+		int countWord = 0;
+		String s;
+		StringBuffer textBuffer = new StringBuffer();
+		String text;
+		String [] textArrays;
 		try{
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
-			while (br.read() != -1) {
-		        String s = br.readLine();
+			while ((s = br.readLine()) != null) {		         
+		        textBuffer.append(s);
+		        textBuffer.append(" ");
 				countChar += s.length();	 
 			}
+			text = textBuffer.toString();
+			text = text.toLowerCase();
+			text = text.replaceAll("[^A-Za-z0-9]", " ");
+			textArrays = text.split("\\s+");
+			countWord = textArrays.length;
+			for (int i = 0;i < textArrays.length;i ++) {
+				if(textArrays[i].length() >= 4) {
+					for (int j = 0;j < 4;j ++) {
+						if(!Character.isLetter(textArrays[i].charAt(j))) {
+							countWord--;
+							break;
+						}
+					}
+				}
+			}
+	                
+	            {
+			}
 			System.out.println(countChar);
+			System.out.println(countWord);
 	    }
 	    catch (IOException e) {
 	        e.printStackTrace();
 	    }
 	}
 }
+
