ebd4183ab75cf75213e2d598c1df3b33adf62325
liangyangxin <965720407@qq.com>
2021-03-02 17:52:50 +0800

    新增统计文件中各单词的出现次数（频率最高的10个）（

--- 221801130/src/WordCount.java
+++ 221801130/src/WordCount.java
@@ -5,6 +5,12 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class WordCount {
@@ -14,11 +20,14 @@ public class WordCount {
 		int countChar = 0;
 		int countWord = 0;
 		int countLine = 0;
+		int num=0;
+		boolean isWord = true;
 		String s;
 		StringBuffer textBuffer = new StringBuffer();
 		String text;
 		String [] textArrays;
-		try{
+		Map<String,Integer> map = new HashMap<String, Integer>();
+		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
 			while ((s = br.readLine()) != null) {		         
 		        textBuffer.append(s);
@@ -32,21 +41,47 @@ public class WordCount {
 			textArrays = text.split("\\s+");
 			countWord = textArrays.length;
 			for (int i = 0;i < textArrays.length;i ++) {
-				if(textArrays[i].length() >= 4) {
+				if(textArrays[i].length() < 4) {
+					countWord--;
+				
+				} else {
+					isWord=true;
 					for (int j = 0;j < 4;j ++) {
 						if(!Character.isLetter(textArrays[i].charAt(j))) {
 							countWord--;
+							isWord=false;
 							break;
 						}
 					}
+					if (isWord == true) {
+						int count=0;
+						if (map.get(textArrays[i]) == null) {
+							count=1;
+						} else {
+							count = map.get(textArrays[i]).intValue() + 1;
+						}
+						map.put(textArrays[i],count);	
+					}
 				}
 			}
-	                
-	            {
-			}
+			List<Map.Entry<String,Integer>> lstEntry = new ArrayList<>(map.entrySet());
 			System.out.println(countChar);
 			System.out.println(countWord);
 			System.out.println(countLine);
+			Collections.sort(lstEntry,((o1, o2) -> {
+				if (o1.getValue().equals(o2.getValue()))
+                    return o1.getKey().compareTo(o2.getKey());
+				else
+					return o2.getValue().compareTo(o1.getValue());
+		    }));
+		    Iterator<Map.Entry<String,Integer>> iter = lstEntry.iterator();
+		     while (iter.hasNext()) {
+		    	 Map.Entry<String,Integer> m = (Map.Entry<String,Integer>) iter.next();
+		         System.out.println(m.getKey() + ":" + m.getValue());
+		         num ++;
+		         if (num == 10)
+		        	 break;
+		     }
 	    }
 	    catch (IOException e) {
 	        e.printStackTrace();
