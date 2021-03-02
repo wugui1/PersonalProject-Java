7b7b71ccaf1aa1c4f428abc842068903e879546f
liangyangxin <965720407@qq.com>
2021-03-02 14:32:46 +0800

    完成统计字符功能

--- 221801130/src/WordCount.java
+++ 221801130/src/WordCount.java
@@ -7,12 +7,18 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class a {
+public class WordCount {
 	public static void main(String[] args) throws IOException {
 		Scanner input = new Scanner(System.in);
-		String path =input.next();
+		String path = input.next();
+		int countChar = 0;
 		try{
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+			while (br.read() != -1) {
+		        String s = br.readLine();
+				countChar += s.length();	 
+			}
+			System.out.println(countChar);
 	    }
 	    catch (IOException e) {
 	        e.printStackTrace();
