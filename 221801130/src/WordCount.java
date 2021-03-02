372d6cb600559e7214d7abe449194c6abb827785
liangyangxin <965720407@qq.com>
2021-03-02 21:34:56 +0800

    对WordCount进行代码格式优化并使统计结果输出到文件里

--- 221801130/src/WordCount.java
+++ 221801130/src/WordCount.java
@@ -1,59 +1,66 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class WordCount {
+	private static int countChar = 0;
+	private static int countWord = 0;
+	private static int countLine = 0;
+	private static Map<String,Integer> map;
+	private String text;
 	
+	public WordCount(String fileName) throws IOException {
+		map = new HashMap<String, Integer>();
+        text = readFile(fileName);
+        countChar = Lib.countChar(text);
+        countWord = Lib.countLines(text);
+        countLine = Lib.countWords(text, map);
+    }
+
 	public static void main(String[] args) throws IOException {
 		Scanner input = new Scanner(System.in);
 		String path = input.next();
+		String outPutPath = input.next();
+		WordCount wordCount = new WordCount(path);
+		writeFile(outPutPath);
+		    
+	   
+	}
+	
+	public static String readFile(String path) throws IOException {
 		BufferedReader br = null;
-		int countChar = 0;
-		int countWord = 0;
-		int countLine = 0;
-		int num=0;
-		int s;
 		StringBuffer textBuffer = new StringBuffer();
-		String text;
-		Map<String,Integer> map = new HashMap<String, Integer>();
-		List<Map.Entry<String,Integer>> lstEntry = new ArrayList<>(map.entrySet());
+		String content = null;
+		int s;
 		try {
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
 			while ((s = br.read()) != -1)         
 				textBuffer.append((char)s);			
-			text = textBuffer.toString();
-			countChar=Lib.countChar(text);
-			countLine=Lib.countLines(text);
-			countWord=Lib.countWords(text, map);
-			System.out.println("characters:"+countChar);
-			System.out.println("words:"+countWord);
-			System.out.println("lines:"+countLine);
-			lstEntry=Lib.getSortedList(map);
-		    Iterator<Map.Entry<String,Integer>> iter = lstEntry.iterator();
-		     while (iter.hasNext()) {
-		    	 Map.Entry<String,Integer> m = (Map.Entry<String,Integer>) iter.next();
-		         System.out.println(m.getKey() + ":" + m.getValue());
-		         num ++;
-		         if (num == 10)
-		        	 break;
-		     }
-	    } catch (IOException e) {
+			content = textBuffer.toString();
+		} catch (IOException e) {
 	        e.printStackTrace();
 	    } finally {
 	    	br.close();
-	    	input.close();
 	    }
+		return content;
+	}
+
+	public static void writeFile(String Path) throws IOException {
+	    List<Map.Entry<String,Integer>> lstEntry = new ArrayList<>(map.entrySet());
+	    lstEntry=Lib.getSortedList(map);
+		 Iterator<Map.Entry<String,Integer>> iter = lstEntry.iterator();
+		 int num = 0;
+		 BufferedWriter writer = new BufferedWriter(new FileWriter(Path));
+		 StringBuilder str = new StringBuilder("characters: " + countChar + '\n' + "words: " + countWord + '\n'
+	                + "lines: " + countLine + '\n');
+	     while (iter.hasNext()) {
+	    	 Map.Entry<String,Integer> m = (Map.Entry<String,Integer>) iter.next();
+	         str.append(m.getKey()).append(": ").append(m.getValue()).append("\n");
+	         num ++;
+	         if (num == 10)
+	        	 break;
+	     }
+	     writer.write(str.toString());
+	     writer.close();
 	}
 	
 }
