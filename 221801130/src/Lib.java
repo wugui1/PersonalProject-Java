85422ec68cf873e6fe7bf794ab8e1fd4797731b7
liangyangxin <965720407@qq.com>
2021-03-02 20:48:59 +0800

    将统计单词功能封装分离

--- 221801130/src/Lib.java
+++ 221801130/src/Lib.java
@@ -17,4 +17,38 @@ public class Lib {
         return count;
     }
 	
+	public static int countWords(String text, Map<String, Integer> map) {
+		int countWord=0;
+		boolean isWord = true;
+		String [] textArrays;
+		text = text.toLowerCase();
+		text = text.replaceAll("[^A-Za-z0-9]", " ");
+		textArrays = text.split("\\s+");
+		countWord = textArrays.length;
+		for (int i = 0;i < textArrays.length;i ++) {
+			if(textArrays[i].length() < 4) {
+				countWord--;
+			} else {
+				isWord=true;
+				for (int j = 0;j < 4;j ++) {
+					if(!Character.isLetter(textArrays[i].charAt(j))) {
+						countWord--;
+						isWord=false;
+						break;
+					}
+				}
+				if (isWord == true) {
+					int count=0;
+					if (map.get(textArrays[i]) == null) {
+						count=1;
+					} else {
+						count = map.get(textArrays[i]).intValue() + 1;
+					}
+					map.put(textArrays[i],count);	
+				}
+			}
+		}
+		return countWord;
+	}
+	
 }
--- 221801130/src/WordCount.java
+++ 221801130/src/WordCount.java
@@ -18,55 +18,27 @@ public class WordCount {
 	public static void main(String[] args) throws IOException {
 		Scanner input = new Scanner(System.in);
 		String path = input.next();
+		BufferedReader br = null;
 		int countChar = 0;
 		int countWord = 0;
 		int countLine = 0;
 		int num=0;
-		boolean isWord = true;
 		int s;
 		StringBuffer textBuffer = new StringBuffer();
 		String text;
-		String [] textArrays;
 		Map<String,Integer> map = new HashMap<String, Integer>();
 		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
 			while ((s = br.read()) != -1)         
 				textBuffer.append((char)s);			
 			text = textBuffer.toString();
 			countChar=Lib.countChar(text);
 			countLine=Lib.countLines(text);
-			text = text.toLowerCase();
-			text = text.replaceAll("[^A-Za-z0-9]", " ");
-			textArrays = text.split("\\s+");
-			countWord = textArrays.length;
-			for (int i = 0;i < textArrays.length;i ++) {
-				if(textArrays[i].length() < 4) {
-					countWord--;
-				
-				} else {
-					isWord=true;
-					for (int j = 0;j < 4;j ++) {
-						if(!Character.isLetter(textArrays[i].charAt(j))) {
-							countWord--;
-							isWord=false;
-							break;
-						}
-					}
-					if (isWord == true) {
-						int count=0;
-						if (map.get(textArrays[i]) == null) {
-							count=1;
-						} else {
-							count = map.get(textArrays[i]).intValue() + 1;
-						}
-						map.put(textArrays[i],count);	
-					}
-				}
-			}
-			List<Map.Entry<String,Integer>> lstEntry = new ArrayList<>(map.entrySet());
+			countWord=Lib.countWords(text, map);
 			System.out.println(countChar);
 			System.out.println(countWord);
 			System.out.println(countLine);
+			List<Map.Entry<String,Integer>> lstEntry = new ArrayList<>(map.entrySet());
 			Collections.sort(lstEntry,((o1, o2) -> {
 				if (o1.getValue().equals(o2.getValue()))
                     return o1.getKey().compareTo(o2.getKey());
@@ -81,11 +53,12 @@ public class WordCount {
 		         if (num == 10)
 		        	 break;
 		     }
-	    }
-	    catch (IOException e) {
+	    } catch (IOException e) {
 	        e.printStackTrace();
+	    } finally {
+	    	br.close();
+	    	input.close();
 	    }
 	}
 	
 }
-
